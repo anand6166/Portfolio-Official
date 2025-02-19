de130f573f6dace490d7dc699324a1c9dfaa4a33
anand6166 <amadanapalli6166@gmail.com>
2025-02-18 22:40:30 -0500

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,8 @@ import ContactSection from "./components/ContactSection";
 
 
 const App: React.FC = () => {
-  console.log('App is rendered'); // Debugging line
   return (
     <div className="App">
-      <h1>Debugging: App is Rendering</h1>
       <Header />
       <div className="main-container">
         <NavBar />
